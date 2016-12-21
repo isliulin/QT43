8e94dde98026c8922f71dbcf115dd6a5d396f42a
heyuanjie <heyuanjie>
2016-12-21 09:50:17 +0800

    setting

--- terminal/settingsdialog.cpp
+++ terminal/settingsdialog.cpp
@@ -2,48 +2,39 @@
 **
 ** Copyright (C) 2012 Denis Shienkov <denis.shienkov@gmail.com>
 ** Copyright (C) 2012 Laszlo Papp <lpapp@kde.org>
-** Contact: https://www.qt.io/licensing/
+** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the QtSerialPort module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:BSD$
+** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
 **
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
 **
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 **
 ** $QT_END_LICENSE$
 **
@@ -58,8 +49,6 @@
 
 QT_USE_NAMESPACE
 
-static const char blankString[] = QT_TRANSLATE_NOOP("SettingsDialog", "N/A");
-
 SettingsDialog::SettingsDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SettingsDialog)
@@ -70,14 +59,12 @@ SettingsDialog::SettingsDialog(QWidget *parent) :
 
     ui->baudRateBox->setInsertPolicy(QComboBox::NoInsert);
 
-    connect(ui->applyButton, &QPushButton::clicked,
-            this, &SettingsDialog::apply);
-    connect(ui->serialPortInfoListBox, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
-            this, &SettingsDialog::showPortInfo);
-    connect(ui->baudRateBox,  static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
-            this, &SettingsDialog::checkCustomBaudRatePolicy);
-    connect(ui->serialPortInfoListBox,  static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
-            this, &SettingsDialog::checkCustomDevicePathPolicy);
+    connect(ui->applyButton, SIGNAL(clicked()),
+            this, SLOT(apply()));
+    connect(ui->serialPortInfoListBox, SIGNAL(currentIndexChanged(int)),
+            this, SLOT(showPortInfo(int)));
+    connect(ui->baudRateBox, SIGNAL(currentIndexChanged(int)),
+            this, SLOT(checkCustomBaudRatePolicy(int)));
 
     fillPortsParameters();
     fillPortsInfo();
@@ -97,16 +84,11 @@ SettingsDialog::Settings SettingsDialog::settings() const
 
 void SettingsDialog::showPortInfo(int idx)
 {
-    if (idx == -1)
-        return;
-
-    QStringList list = ui->serialPortInfoListBox->itemData(idx).toStringList();
-    ui->descriptionLabel->setText(tr("Description: %1").arg(list.count() > 1 ? list.at(1) : tr(blankString)));
-    ui->manufacturerLabel->setText(tr("Manufacturer: %1").arg(list.count() > 2 ? list.at(2) : tr(blankString)));
-    ui->serialNumberLabel->setText(tr("Serial number: %1").arg(list.count() > 3 ? list.at(3) : tr(blankString)));
-    ui->locationLabel->setText(tr("Location: %1").arg(list.count() > 4 ? list.at(4) : tr(blankString)));
-    ui->vidLabel->setText(tr("Vendor Identifier: %1").arg(list.count() > 5 ? list.at(5) : tr(blankString)));
-    ui->pidLabel->setText(tr("Product Identifier: %1").arg(list.count() > 6 ? list.at(6) : tr(blankString)));
+    if (idx != -1)
+    {
+        QStringList list = ui->serialPortInfoListBox->itemData(idx).toStringList();
+        ui->descriptionLabel->setText(tr("描述: %1").arg(list.at(1)));
+    }
 }
 
 void SettingsDialog::apply()
@@ -119,28 +101,22 @@ void SettingsDialog::checkCustomBaudRatePolicy(int idx)
 {
     bool isCustomBaudRate = !ui->baudRateBox->itemData(idx).isValid();
     ui->baudRateBox->setEditable(isCustomBaudRate);
-    if (isCustomBaudRate) {
+    if (isCustomBaudRate)
+    {
         ui->baudRateBox->clearEditText();
         QLineEdit *edit = ui->baudRateBox->lineEdit();
         edit->setValidator(intValidator);
     }
 }
 
-void SettingsDialog::checkCustomDevicePathPolicy(int idx)
-{
-    bool isCustomPath = !ui->serialPortInfoListBox->itemData(idx).isValid();
-    ui->serialPortInfoListBox->setEditable(isCustomPath);
-    if (isCustomPath)
-        ui->serialPortInfoListBox->clearEditText();
-}
-
 void SettingsDialog::fillPortsParameters()
 {
-    ui->baudRateBox->addItem(QStringLiteral("115200"), QSerialPort::Baud115200);
     ui->baudRateBox->addItem(QStringLiteral("9600"), QSerialPort::Baud9600);
     ui->baudRateBox->addItem(QStringLiteral("19200"), QSerialPort::Baud19200);
     ui->baudRateBox->addItem(QStringLiteral("38400"), QSerialPort::Baud38400);
-    ui->baudRateBox->addItem(tr("Custom"));
+    ui->baudRateBox->addItem(QStringLiteral("115200"), QSerialPort::Baud115200);
+    ui->baudRateBox->addItem(QStringLiteral("自定义"));
+    ui->baudRateBox->setCurrentIndex(3);
 
     ui->dataBitsBox->addItem(QStringLiteral("5"), QSerialPort::Data5);
     ui->dataBitsBox->addItem(QStringLiteral("6"), QSerialPort::Data6);
@@ -148,56 +124,66 @@ void SettingsDialog::fillPortsParameters()
     ui->dataBitsBox->addItem(QStringLiteral("8"), QSerialPort::Data8);
     ui->dataBitsBox->setCurrentIndex(3);
 
-    ui->parityBox->addItem(tr("None"), QSerialPort::NoParity);
-    ui->parityBox->addItem(tr("Even"), QSerialPort::EvenParity);
-    ui->parityBox->addItem(tr("Odd"), QSerialPort::OddParity);
-    ui->parityBox->addItem(tr("Mark"), QSerialPort::MarkParity);
-    ui->parityBox->addItem(tr("Space"), QSerialPort::SpaceParity);
+    ui->parityBox->addItem(QStringLiteral("None"), QSerialPort::NoParity);
+    ui->parityBox->addItem(QStringLiteral("Even"), QSerialPort::EvenParity);
+    ui->parityBox->addItem(QStringLiteral("Odd"), QSerialPort::OddParity);
+    ui->parityBox->addItem(QStringLiteral("Mark"), QSerialPort::MarkParity);
+    ui->parityBox->addItem(QStringLiteral("Space"), QSerialPort::SpaceParity);
 
     ui->stopBitsBox->addItem(QStringLiteral("1"), QSerialPort::OneStop);
+#ifdef Q_OS_WIN
+    ui->stopBitsBox->addItem(QStringLiteral("1.5"), QSerialPort::OneAndHalfStop);
+#endif
     ui->stopBitsBox->addItem(QStringLiteral("2"), QSerialPort::TwoStop);
 
-    ui->flowControlBox->addItem(tr("None"), QSerialPort::NoFlowControl);
-    ui->flowControlBox->addItem(tr("RTS/CTS"), QSerialPort::HardwareControl);
-    ui->flowControlBox->addItem(tr("XON/XOFF"), QSerialPort::SoftwareControl);
+    ui->flowControlBox->addItem(QStringLiteral("None"), QSerialPort::NoFlowControl);
+    ui->flowControlBox->addItem(QStringLiteral("RTS/CTS"), QSerialPort::HardwareControl);
+    ui->flowControlBox->addItem(QStringLiteral("XON/XOFF"), QSerialPort::SoftwareControl);
 }
 
 void SettingsDialog::fillPortsInfo()
 {
-    ui->serialPortInfoListBox->clear();
+    static const QString blankString = QObject::tr("N/A");
     QString description;
-    QString manufacturer;
-    QString serialNumber;
-    const auto infos = QSerialPortInfo::availablePorts();
-    for (const QSerialPortInfo &info : infos) {
+    QStringList list;
+
+    ui->serialPortInfoListBox->clear();
+
+    list << QString(tr("刷新"))
+         << QString(tr("点击更新列表"));
+    ui->serialPortInfoListBox->addItem(list.first(), list);
+
+    foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
+    {
         QStringList list;
         description = info.description();
-        manufacturer = info.manufacturer();
-        serialNumber = info.serialNumber();
+
         list << info.portName()
-             << (!description.isEmpty() ? description : blankString)
-             << (!manufacturer.isEmpty() ? manufacturer : blankString)
-             << (!serialNumber.isEmpty() ? serialNumber : blankString)
-             << info.systemLocation()
-             << (info.vendorIdentifier() ? QString::number(info.vendorIdentifier(), 16) : blankString)
-             << (info.productIdentifier() ? QString::number(info.productIdentifier(), 16) : blankString);
+             << (!description.isEmpty() ? description : blankString);
 
         ui->serialPortInfoListBox->addItem(list.first(), list);
     }
 
-    ui->serialPortInfoListBox->addItem(tr("Custom"));
+    if (ui->serialPortInfoListBox->count() != 1)
+    {
+        ui->serialPortInfoListBox->setCurrentIndex(1);
+    }
 }
 
 void SettingsDialog::updateSettings()
 {
     currentSettings.name = ui->serialPortInfoListBox->currentText();
 
-    if (ui->baudRateBox->currentIndex() == 4) {
+    if (ui->baudRateBox->currentIndex() == 4)
+    {
         currentSettings.baudRate = ui->baudRateBox->currentText().toInt();
-    } else {
+    }
+    else
+    {
         currentSettings.baudRate = static_cast<QSerialPort::BaudRate>(
                     ui->baudRateBox->itemData(ui->baudRateBox->currentIndex()).toInt());
     }
+
     currentSettings.stringBaudRate = QString::number(currentSettings.baudRate);
 
     currentSettings.dataBits = static_cast<QSerialPort::DataBits>(
@@ -215,6 +201,12 @@ void SettingsDialog::updateSettings()
     currentSettings.flowControl = static_cast<QSerialPort::FlowControl>(
                 ui->flowControlBox->itemData(ui->flowControlBox->currentIndex()).toInt());
     currentSettings.stringFlowControl = ui->flowControlBox->currentText();
+}
 
-    currentSettings.localEchoEnabled = ui->localEchoCheckBox->isChecked();
+void SettingsDialog::on_serialPortInfoListBox_activated(int index)
+{
+    if (index == 0)
+    {
+        fillPortsInfo();
+    }
 }
